0e52cd0e3bb96b6d5fa9ef15da267dd860ca22d2
Thomas Michiels <thomasmichiels0@gmail.com>
2025-11-27 11:01:37 +0100

    Nav bar change

--- app/content/navigation.ts
+++ app/content/navigation.ts
@@ -2,6 +2,7 @@ export const NAV = [
     {name: "Home", href: "/"},
     {name: "Woningtypes", href: "/woningtypes"},
     {name: "Financieel", href: "/financieel"},
+    {name: "Wooncomfort", href: "/wooncomfort"},
     {name: "Veelgestelde vragen", href: "/faq"},
     {name: "Actie ondernemen", href: "/actie-ondernemen"},
 ];
